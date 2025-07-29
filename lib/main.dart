f58cee0efb8bb4df3e0b8f138ab90dd79c058eb2
napattapongy66 <napattapongy66@nu.ac.th>
2025-07-29 13:52:57 +0700

    My_First_App

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart'; // Import ไลบรารี Material ของ Flutter
+
+void main() {
+  // จุดเริ่มต้นของแอป Flutter
+  runApp(const MyApp()); // บอก Flutter ว่าวิดเจ็ตใดคือรากฐานของแอป
+}
+
+// MyApp โดยทั่วไปคือวิดเจ็ตรากฐานของแอปพลิเคชันของคุณ
+class MyApp extends StatelessWidget {
+  const MyApp({super.key}); // Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    // เมธอด build() ส่งคืนโครงสร้างต้นไม้วิดเจ็ตสำหรับวิดเจ็ตนี้
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true, // ใช้ Material Design เวอร์ชันล่าสุด
+      ),
+      title: 'Flutter Demo',
+      home: const MyHomePage(),
+    );
+  }
+}
+
+// ตัวอย่างวิดเจ็ตที่ส่งคืนโดย property 'home' ของ MaterialApp
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // ให้โครงสร้างแอปพื้นฐาน
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        backgroundColor: Colors.blue, // ปรับแต่งสี AppBar
+      ),
+      body: Center(
+        // พื้นที่เนื้อหาหลัก
+        child: Container(
+          width: 300.0,
+          height: 150.0,
+          decoration: BoxDecoration(
+            color: Colors.amber,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Center(
+            child: Image.asset(
+              'assets/images/cat.jpg', // URL รูปภาพตัวอย่าง
+              width: 100,
+              height: 50,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('FAB Pressed!');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
